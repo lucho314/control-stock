7d0239aa1f1c4d6ed70e41c984285ba8ca634b79
Luciano Zapata <lzapata@ater.gob.ar>
2024-09-20 21:26:12 -0300

    Agregar lógica para crear o actualizar proveedores

---
+++ src/actions/provider/create-update.ts
@@ -0,0 +1,45 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import { z } from "zod";
+
+const providerSchema = z.object({
+  id: z.string().uuid().optional().nullable(),
+  nombre: z.string().min(3).max(255),
+  contacto: z.string().min(3).max(255),
+  direccion: z.string().min(3).max(255),
+});
+
+export const createUpdateProvider = async (formData: FormData) => {
+  const data = Object.fromEntries(formData);
+  const providerParse = providerSchema.safeParse(data);
+
+  if (!providerParse.success) {
+    console.log(providerParse.error);
+    return { ok: false };
+  }
+
+  const provider = providerParse.data;
+
+  const { id, ...rest } = provider;
+
+  if (id) {
+    // Actualizar
+    const provider = await prisma.proveedores.update({
+      where: { id },
+      data: {
+        ...rest,
+      },
+    });
+
+    return { ok: true, provider };
+  }
+
+  const newProvider = await prisma.proveedores.create({
+    data: {
+      ...rest,
+    },
+  });
+
+  return { ok: true, provider: newProvider };
+};
