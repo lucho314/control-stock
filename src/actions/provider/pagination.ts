14386868054eb85d4a97d841cd4f15e6a0fa1f03
Luciano Zapata <lzapata@ater.gob.ar>
2024-09-26 18:23:35 -0300

    Actualizar dependencias y corregir error de búsqueda en proveedores

--- src/actions/provider/pagination.ts
+++ src/actions/provider/pagination.ts
@@ -5,11 +5,13 @@ import prisma from "@/lib/prisma";
 interface PaginationOptions {
   page?: number;
   take?: number;
+  q?: string;
 }
 
 export const getPaginatedProvider = async ({
   page = 1,
   take = 12,
+  q,
 }: PaginationOptions) => {
   if (isNaN(Number(page))) page = 1;
   if (page < 1) page = 1;
@@ -19,6 +21,12 @@ export const getPaginatedProvider = async ({
     const providers = await prisma.proveedores.findMany({
       take: take,
       skip: (page - 1) * take,
+      where: {
+        nombre: {
+          contains: q,
+          mode: "insensitive",
+        },
+      },
     });
 
     // 2. Obtener el total de páginas
