a26c70aeeab14b7e07d6ad5ab3c20c41fa902cb3
Luciano Zapata <luciano.zapata314@gmail.com>
2024-10-03 23:08:06 -0300

    Refactor image domain in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['imgs.search.brave.com']
+        remotePatterns: [
+            {
+                hostname: 'https://imgs.search.brave.com',
+            }
+        ],
     }
 };
 
