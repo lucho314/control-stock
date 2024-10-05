001cecca9efd6169df9245fc0853f0ae0c2b617f
Luciano Zapata <luciano.zapata314@gmail.com>
2024-10-05 10:35:51 -0300

    Refactor next.config.mjs to customize Webpack externals for server-side rendering

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,22 @@ const nextConfig = {
         remotePatterns: [
             {
                 hostname: 'imgs.search.brave.com',
-            }
+            },
         ],
-    }
+    },
+
+    // Personalización de Webpack
+    webpack: (config, { isServer }) => {
+        // Si estamos en el lado del servidor, agregar '@sparticuz/chromium' a externals
+        if (isServer) {
+            config.externals = [
+                '@sparticuz/chromium', // Agregar a externals
+                ...(config.externals || []), // Extender cualquier otra configuración de externals
+            ];
+        }
+
+        return config; // Devolver la configuración modificada
+    },
 };
 
 export default nextConfig;
