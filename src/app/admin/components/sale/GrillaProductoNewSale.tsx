f783f301717ba477a37980e660b539c0c76e93c1
Luciano Zapata <luciano.zapata314@gmail.com>
2024-10-05 18:04:35 -0300

    Refactor input fields in GrillaProductoNewSale component to accept uppercase values

--- src/app/admin/components/sale/GrillaProductoNewSale.tsx
+++ src/app/admin/components/sale/GrillaProductoNewSale.tsx
@@ -90,7 +90,7 @@ export const GrillaProductoNewSale = () => {
                 variant="sm"
                 name="nombre"
                 defaultValue={producto.nombre!}
-                className="w-full p-2"
+                className="w-full p-2 uppercase"
               />
             </TableCell>
             <TableCell>{producto.precio || "$0.00"}</TableCell>
@@ -115,7 +115,7 @@ export const GrillaProductoNewSale = () => {
             <Input
               variant="sm"
               name="codigo"
-              className="w-full p-2"
+              className="w-full p-2 uppercase"
               onKeyDown={handleKeydown}
               loading={loading}
               ref={inputCodigoRef}
