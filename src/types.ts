a5219e479ac9d8a686b24ed4492406c59ca03e4a
Luciano Zapata <luciano.zapata314@gmail.com>
2024-10-18 11:41:45 -0300

    Refactor createUpdateProduct to use typed product object

--- src/types.ts
+++ src/types.ts
@@ -11,9 +11,15 @@ export type Provider = Omit<proveedores, "id" | "activo"> & {
   id?: string;
 };
 
-export type Producto = Omit<productos, "id" | "precio"> & {
+export type Producto = Omit<
+  productos,
+  "id" | "precio" | "precio_costo" | "porcentaje_ganancia" | "codigo_de_barras"
+> & {
   id?: string;
-  precio: number;
+  precio: number | null;
+  precio_costo: number | null;
+  porcentaje_ganancia: number | null;
+  codigo_de_barras?: string | null;
 };
 
 export type ProductoVenta = Omit<venta_producto, "id"> & {
