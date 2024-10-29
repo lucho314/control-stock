8ab1243f953d5877f1a6369d8f881433d208bc41
Luciano Zapata <luciano.zapata314@gmail.com>
2024-10-29 19:06:17 -0300

    Refactor createUpdateProduct to remove commented out code for uploading images

--- src/actions/product/create-update-product.ts
+++ src/actions/product/create-update-product.ts
@@ -102,27 +102,3 @@ export const createUpdateProduct = async (
     return { ok: false, error: "Error desconocido", type: "unknown" };
   }
 };
-
-// const uploadImages = async (images: File[]) => {
-//   try {
-//     const uploadPromises = images.map(async (image) => {
-//       try {
-//         const buffer = await image.arrayBuffer();
-//         const base64Image = Buffer.from(buffer).toString("base64");
-
-//         return cloudinary.uploader
-//           .upload(`data:image/png;base64,${base64Image}`)
-//           .then((r) => r.secure_url);
-//       } catch (error) {
-//         console.log(error);
-//         return null;
-//       }
-//     });
-
-//     const uploadedImages = await Promise.all(uploadPromises);
-//     return uploadedImages;
-//   } catch (error) {
-//     console.log(error);
-//     return null;
-//   }
-// };
