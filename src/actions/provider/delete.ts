4b32d93cd4c9e096338765a8c3d326bd3bf22337
Luciano Zapata <luciano.zapata314@gmail.com>
2024-10-29 19:20:07 -0300

    Refactor deleteProvider to check admin permissions before executing the logic

--- src/actions/provider/delete.ts
+++ src/actions/provider/delete.ts
@@ -1,9 +1,16 @@
 "use server";
 
+import { isAdmin } from "@/lib/middleware";
 import prisma from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
 
 export const deleteProvider = async (id: string) => {
+  const [error, userId] = await isAdmin();
+
+  if (error) {
+    console.error(error);
+    return { ok: false, error };
+  }
   await prisma.proveedores.update({
     where: {
       id,
